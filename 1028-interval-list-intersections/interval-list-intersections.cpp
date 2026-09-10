6090a1e10f73b77b1ef0d9f3a161b2d24a5af482
Ayush Verma <ayushjee2016@gmail.com>
2026-09-10 12:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (23.43%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -11,18 +11,15 @@ public:
             // 1. Find intersection
             int s1 = max(firstList[i][0], secondList[j][0]);
             int e1 = min(firstList[i][1], secondList[j][1]);
-
+            // 2. Store intersection if it exists
             if (s1 <= e1)
                 ans.push_back({s1, e1});
-
+            // 3. Move the interval that ends first
             if (firstList[i][1] < secondList[j][1])
                 i++;
             else
                 j++;
-
-            // 2. Store intersection if it exists
-
-            // 3. Move the interval that ends first
+            
         }
 
         return ans;
